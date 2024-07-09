f28e8e439881ee1f9686aed9fc891ce84426fb01
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2024-07-09 18:54:42 -0300

    Add Witness robot

---
+++ src/destroyer/Witness.java
@@ -0,0 +1,18 @@
+package destroyer;
+import robocode.*;
+
+import java.awt.*;
+public class Witness extends AdvancedRobot {
+  /**
+   * Witness - a robot by lucas b.
+   */
+  @Override
+  public void run() {
+    setBodyColor(Color.magenta);
+    setGunColor(Color.cyan);
+    setRadarColor(Color.yellow);
+    setBulletColor(Color.orange);
+  }
+  
+  
+}
