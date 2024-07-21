8ad42702bbbdc141569d30f2f49b2f2dc70bcfd9
Niltinho77 <nilton.cn1992@aluno.ifsc.edu.br>
2024-07-21 18:08:22 -0300

    last uptade Ultron and Bumblebee

--- src/exterminators/Bumblebee.java
+++ src/exterminators/Bumblebee.java
@@ -1,71 +1,52 @@
 package exterminators;
 
 import robocode.*;
+import robocode.util.Utils;
+
 import java.awt.Color;
 
 public class Bumblebee extends AdvancedRobot {
 
     @Override
     public void run() {
-        setBodyColor(Color.yellow); // Define a cor do corpo do robô como cinza escuro
-        setGunColor(Color.yellow); // Define a cor da arma do robô como vermelha
-        setRadarColor(Color.YELLOW); // Define a cor do radar do robô como amarela
-        setBulletColor(Color.yellow); // Define a cor das balas disparadas pelo robô como magenta
-        setScanColor(Color.yellow); // Define a cor do feixe de escaneamento do robô como ciano
-        // Configurações para que a arma e o radar girem independentemente do corpo do robô
+        setBodyColor(Color.yellow);
+        setGunColor(Color.yellow);
+        setRadarColor(Color.YELLOW);
+        setBulletColor(Color.yellow);
+        setScanColor(Color.yellow);
+
         setAdjustRadarForRobotTurn(true);
         setAdjustGunForRobotTurn(true);
         setAdjustRadarForGunTurn(true);
 
         while (true) {
-            // Gira o radar continuamente para a direita
             setTurnRadarRight(360);
-            // Move para frente e para trás aleatoriamente
             setAhead(100 * (Math.random() - 0.5));
-            // Gira aleatoriamente
             setTurnRight(90 * (Math.random() - 0.5));
-            // Executa todas as ações pendentes
             execute();
         }
     }
-
     @Override
-    public void onScannedRobot(ScannedRobotEvent evento) {
-        double potenciaBala = Math.min(3.0, getEnergy()); // Define a potência do tiro com base na energia do robô
-        double meuX = getX();
-        double meuY = getY();
-        double anguloAbsoluto = getHeadingRadians() + evento.getBearingRadians();
-        double inimigoX = meuX + evento.getDistance() * Math.sin(anguloAbsoluto);
-        double inimigoY = meuY + evento.getDistance() * Math.cos(anguloAbsoluto);
-        double cabecaInimigo = evento.getHeadingRadians();
-        double velocidadeInimigo = evento.getVelocity();
+    public void onScannedRobot(ScannedRobotEvent e) {
+        double anguloAbsoluto = e.getBearingRadians() + getHeadingRadians();
+        double distancia = e.getDistance();
 
-        // Previsão da posição do inimigo
-        double deltaTempo = 0;
-        double previsaoX = inimigoX;
-        double previsaoY = inimigoY;
-        double velocidadeBala = 20 - 3 * potenciaBala; // Velocidade da bala
+        setTurnGunRightRadians(Utils.normalRelativeAngle(anguloAbsoluto - getGunHeadingRadians()));
 
-        while ((++deltaTempo) * velocidadeBala < Math.hypot(meuX - previsaoX, meuY - previsaoY)) {
-            previsaoX += Math.sin(cabecaInimigo) * velocidadeInimigo;
-            previsaoY += Math.cos(cabecaInimigo) * velocidadeInimigo;
+        if (distancia < 200 && getEnergy() > 2) {
+            setFire(3);
+        } else {
+            setFire(1);
         }
 
-        double theta = Math.atan2(previsaoX - meuX, previsaoY - meuY); // Calcula o ângulo absoluto
-
-        // Gira o radar, a arma e atira
-        setTurnRadarRightRadians(normalizarAngulo(anguloAbsoluto - getRadarHeadingRadians()));
-        setTurnGunRightRadians(normalizarAngulo(theta - getGunHeadingRadians()));
-        setFire(potenciaBala);
-
-        // Move o robô
-        setTurnRight(evento.getBearing());
+        double anguloMovimento = Utils.normalRelativeAngle(anguloAbsoluto + Math.PI / 2 - 0.6);
+        setTurnRightRadians(Utils.normalRelativeAngle(anguloMovimento - getHeadingRadians()));
         setAhead(100);
+        execute();
     }
 
     @Override
     public void onHitByBullet(HitByBulletEvent evento) {
-        // Ajusta a direção do robô para evitar mais tiros
         setTurnRight(90 - evento.getBearing());
         setAhead(150); // Move para frente para escapar
     }
@@ -74,13 +55,8 @@ public class Bumblebee extends AdvancedRobot {
     public void onHitWall(HitWallEvent evento) {
         back(50); // Move para trás para se afastar da parede
         setTurnRight(90); // Gira o robô para evitar a parede
-        ahead(50); // Move para frente novamente
+        ahead(50);
     }
 
-    // Método utilitário para normalizar um ângulo para o intervalo [-PI, PI]
-    private double normalizarAngulo(double angulo) {
-        while (angulo > Math.PI) angulo -= 2 * Math.PI;
-        while (angulo < -Math.PI) angulo += 2 * Math.PI;
-        return angulo;
-    }
+
 }
--- src/exterminators/ULTRON.java
+++ src/exterminators/ULTRON.java
@@ -5,29 +5,26 @@ import robocode.util.Utils;
 import java.awt.Color;
 
 public class ULTRON extends AdvancedRobot {
+    @Override
     public void run() {
         setBodyColor(Color.darkGray);
-        setGunColor(Color.darkGray);
-        setRadarColor(Color.YELLOW);
-        setBulletColor(Color.MAGENTA);
+        setGunColor(Color.black);
+        setRadarColor(Color.darkGray);
+        setBulletColor(Color.yellow);
         setScanColor(Color.CYAN);
-
 
         setAdjustGunForRobotTurn(true);
         setAdjustRadarForRobotTurn(true);
 
-
         while (true) {
-
             setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
             execute();
         }
     }
 
+    @Override
     public void onScannedRobot(ScannedRobotEvent e) {
-
         double anguloAbsoluto = e.getBearingRadians() + getHeadingRadians();
-
         double distancia = e.getDistance();
 
         setTurnGunRightRadians(Utils.normalRelativeAngle(anguloAbsoluto - getGunHeadingRadians()));
@@ -38,25 +35,23 @@ public class ULTRON extends AdvancedRobot {
             setFire(1);
         }
 
-
-        double anguloMovimento = Utils.normalRelativeAngle(anguloAbsoluto + Math.PI / 2 - 1.0
-        );
-
+        double anguloMovimento = Utils.normalRelativeAngle(anguloAbsoluto + Math.PI / 2 - 0.6);
         setTurnRightRadians(Utils.normalRelativeAngle(anguloMovimento - getHeadingRadians()));
+        setAhead(100);
+        execute();
+    }
+
+    @Override
+    public void onHitByBullet(HitByBulletEvent e) {
+        setTurnRight(90 - e.getBearing());
         setAhead(100);
     }
 
-    // Método chamado quando o robô é atingido por uma bala
-    public void onHitByBullet(HitByBulletEvent e) {
-        // Ajusta a direção do robô para evitar mais tiros
-        setTurnRight(90 - e.getBearing());
-        setAhead(100); // Move o robô para frente
-    }
-
-    // Método chamado quando o robô colide com uma parede
+    @Override
     public void onHitWall(HitWallEvent e) {
-        setBack(50); // Move o robô para trás para se afastar da parede
-        setTurnRight(45); // Ajusta a direção do robô para evitar futuras colisões
-        setAhead(100); // Move o robô para frente
+        setBack(50);
+        setTurnRight(45);
+        setAhead(100);
+        execute();
     }
 }
